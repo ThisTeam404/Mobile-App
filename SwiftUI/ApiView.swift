c9ea7d559ac39ab7c4eab13be8ae0e67c5d0ee2d
kjse <75707747+kjse@users.noreply.github.com>
2022-04-17 22:05:27 -0700

    TM404-99
    Previously TextFieldView.swift; user inputs API key thats sent and recognized by the server

---
+++ SwiftUI/ApiView.swift
@@ -0,0 +1,62 @@
+//
+//  NotesView.swift
+//  GoogleLoginOption
+//
+//  Created by Matthew Murata and Kaily Estepa on 3/22/22.
+//
+
+import SwiftUI
+
+class setApiKey: ObservableObject{
+    @Published var enterApiKey = "insertApiKey"
+}
+
+struct ApiView: View {
+    
+    @ObservedObject var setApi: setApiKey
+    @State var apiKey = ""
+    
+    var body: some View {
+        ZStack{
+            Image("Background")
+                .resizable()
+                .scaledToFill()
+                .opacity(0.30)
+                .ignoresSafeArea()
+            VStack(){
+                Text("Enter API Key")
+                    .frame(width: 330, height: 50, alignment: .leading)
+                
+                TextField("API Key", text: $apiKey)
+                    .frame(width: 300, height: 20, alignment: .leading)
+                    .padding()
+                    .border(.blue)
+                    .background(.black.opacity(0.1))
+                
+                //assign
+                Button("Send", action:{
+                    sendApiKey()
+                }).frame(width: 300, height: 40, alignment: .center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+                    .cornerRadius(15)
+         
+                Spacer()
+            }.offset(y:10)
+        }
+            
+    }
+    func sendApiKey(){
+        setApi.enterApiKey = apiKey
+        print("\(setApi.enterApiKey) entered")
+        //apiKey = ""
+              
+    }
+    
+}
+/*
+struct ApiView_Previews: PreviewProvider {
+    static var previews: some View {
+        ApiView()
+    }
+}*/
